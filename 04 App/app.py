c9f1d2e0488a91b0926b5693684e55b9450ea5ca
lucas-hara <lucasrhm@hotmail.com>
2022-04-11 19:41:07 -0300

    update

--- 04 App/app.py
+++ 04 App/app.py
@@ -8,7 +8,7 @@ print("\n\nSTARTING....")
 # Configura página
 st.set_page_config(
     page_title="Recomendação de produtos de skincare",
-    layout='wide',
+    # layout='wide',
     #page_icon= 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/facebook/304/lotion-bottle_1f9f4.png'
     #page_icon= 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/google/313/lotion-bottle_1f9f4.png'
     page_icon='https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/310/lotion-bottle_1f9f4.png'
@@ -16,33 +16,54 @@ st.set_page_config(
 
 
 @st.cache
+# Função para carregamento da matriz de similaridade e do dataset
 def load_data():
-    sim_matrix = np.load("Similarity Matrix.npy")
-    df = pd.read_csv("Exploded Dataframe.csv")
+    sim_matrix = np.load(
+        "../03 Notebook e datasets gerados/Similarity Matrix.npy")
+    df = pd.read_csv(
+        "../03 Notebook e datasets gerados/Exploded Dataframe.csv")
 
-    df.drop(['Brand', 'Name'], axis=1, inplace=True)
-    cols = df.columns.to_list()
-    cols.remove("Full Name")
-    cols = ["Full Name"] + cols
-    df = df[cols]
+    # Remove os itens do formato de lista para determinadas colunas
+    for col in ["Skin Type", "Skin Concerns", "Formulation", "Skincare By Age", "Ingredients", "Function"]:
+        df[col] = df[col].apply(
+            lambda x: ", ".join(eval(x))
+        )
 
     return sim_matrix, df
 
 
-def get_top5(prod_id):
-    similar_list = list(enumerate(sim_matrix[prod_id][0]))
+# Criação do algoritmo de busca dos produtos similares
+def busca_similares(id_produto, sim_matrix):
+
+    # Encontra os valores similares do produto indicado, colocando em ordem decrescente
+    similar_list = list(enumerate(sim_matrix[id_produto][0]))
     sorted_similar_list = sorted(
         similar_list, key=lambda x: x[1], reverse=True)
+
+    # Seleciona os 5 produtos mais similares (além do próprio produto)
     top_5 = sorted_similar_list[0:6]
 
-    top_5_idx, top_5_sim = zip(*top_5)
-    top_5_idx = list(top_5_idx)
-    top_5_sim = [f"{(100*x):.1f} %" for x in top_5_sim]
+    # Retorna a lista com os índices e valores de similaridade dos 5 produtos mais semelhantes
+    top_5_idx, top_5_values = zip(*top_5)
 
-    df_top5 = df.iloc[list(top_5_idx)].copy()
-    df_top5['Similarity'] = top_5_sim
+    # Cria um dataframe com os 5 produtos mais semelhantes, baseado no dataframe original
+    df_top_5 = df.iloc[list(top_5_idx)].copy()
+    df_top_5['Similarity'] = [f"{(100*x).round(1)}%" for x in top_5_values]
 
-    return df_top5
+    return df_top_5
+
+
+# Função para a criação de uma matriz resumida
+def get_resumed_df(df):
+    df_resumed = pd.DataFrame()
+    df_resumed['Full Name'] = df['Full Name']
+    for col in df.drop('Full Name', axis=1):
+        df_resumed[col] = df[col]
+
+    df_resumed.drop(["Brand", "Name", "Product URL",
+                    "Image URL"], axis=1, inplace=True)
+
+    return df_resumed
 
 
 # Carrega dados
@@ -63,65 +84,45 @@ product = st.selectbox("Produto:", df['Full Name'].sort_values())
 
 prod_id = df[df['Full Name'] == product].index
 
-df_top5 = get_top5(prod_id)
+df_top_5 = busca_similares(prod_id, sim_matrix)
+df_resumed = get_resumed_df(df_top_5)
+
+with st.expander("Mais informações..."):
+    st.table(df_resumed[:1].T.astype(str))
 
 # Mostra os produtos recomendados
-for idx, (index, row) in enumerate(df_top5.iterrows()):
-    st.write("###")
+for idx, (index, row) in enumerate(df_top_5.iterrows()):
 
     if idx == 0:
         continue
 
-    url = "https://image-optimizer-reg.production.sephora-asia.net/images/product_images/closeup_1_Product_9336328011956-Alpha-H-Beauty-Sleep-Power-Peel-50ml_a227bb1064e2bb9d148a3d15d0abd4e654a8cf15_1617778706.png"
+    st.write("#### ━━━")
 
-    if idx == 1:
-        base_delta = float(row['Similarity'].split(" ")[0])
-        delta = ""
+    # Definição das colunas
+    col1, col2, col3 = st.columns([0.1, 0.3, 0.7])
 
-    else:
-        delta_i = float(row['Similarity'].split(" ")[0])
-        delta = f"{(delta_i - base_delta):.1f}%"
+    # Coluna 1: ranking
+    col1.metric("Rank", idx)
 
-    col1, col2 = st.columns([1, 4])
+    # Coluna 2: imagem do produto
+    col2.image(row["Image URL"])
 
-    col1.image(url)
-    #col1.markdown('<p style="color:Blue; font-size: 20px;">Original image</p>',unsafe_allow_html=True)
-    # col2.metric(
-    #     label=row['Full Name'],
-    #     #label="[Teste](https://www.google.com)",
-    #     value=row['Similarity'],
-    #     #delta= delta
-    # )
-    with col2.container():
+    # Coluna 3: Informações do produto
+    with col3.container():
 
         st.write(f'''
-            <p style="bold: True; color: white; font-size: 22px;"> 
-                <b>{row["Full Name"]}</b>
-            </p>''', unsafe_allow_html=True)
+            <p style="bold: True; color: #454545; font-size: 22px;"> 
+                <b>{row["Brand"]}</b><br>
+                {row["Name"]}
 
+            </p>''', unsafe_allow_html=True)
 
         st.write(f'''
-            <p style="bold: True; color: white; font-size: 30px;"> 
-                {row["Similarity"]}
+            <p style="bold: True; color: gray; font-size: 18px;"> 
+                Similaridade: {row["Similarity"]}
             </p>''', unsafe_allow_html=True)
 
-        # st.write(f'''
-        #     <p style="bold: True; color: red; font-size: 30px;"> 
-        #         {delta}
-        #     </p>''', unsafe_allow_html=True)
-
-
-
-        st.caption(f"[LINK]()")
-        # st.write(f'''
-        #     <p style="bold: True; color: white; font-size: 18px;"> 
-        #         <a href="https://www.google.com.br">LINK
-        #     </p>''', unsafe_allow_html=True)
+        st.caption(f"[LINK DO PRODUTO]({row['Product URL']})")
 
     with st.expander("Mais informações..."):
-        st.table(row)
-
-
-def get_link(prod_name):
-    prod_name.replace("%", "percent")
-    prod_name.replace("|", "")
+        st.table(df_resumed.iloc[idx])
